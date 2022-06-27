2878758e0724103730bf5ca567ee5c4ca9d7062c
lauryn-21 <100782561+lauryn-21@users.noreply.github.com>
2022-06-27 21:50:52 +0300

    this is my first commit

---
+++ category/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from.models import Category
+
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+   prepopulated_fields ={'slug':('category_name',)}
+   list_display =('slug','category_name')
+   
+   
+admin.site.register(Category, CategoryAdmin)
+
